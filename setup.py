0a0121019944311ce577489afd512e6d8ed36dd1
tarek <tarek@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-05-07 09:29:22 +0000

    fixed description

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ version = '0.1.0'
 setup(name='collective.psc.externalstorage',
       version=version,
       description="External storage for PloneSoftwareCenter",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.txt").read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
